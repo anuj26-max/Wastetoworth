cfecf4459b6bd5a79368331101fdd28f4e155000
Anuj Kumar Singh <as9703684@gmail.com>
2026-09-11 17:32:22 +0530

    fix(android): disable ndkVersion to resolve build and sdkmanager timeout

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.waste2worth.waste2worth"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
